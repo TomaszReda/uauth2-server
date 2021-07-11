469e623f25f0c8ee7f89b381ef1265e1ba46e169
treda <tomekreda16@gmail.com>
2021-07-11 13:30:59 +0200

    Initial commit

---
+++ src/main/java/com/example/oauth2/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.example.oauth2.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
